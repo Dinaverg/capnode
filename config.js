6e1889c2ea826703b616cab9bb68c9a8ac000936
Darryl Williams <kirbydinaverg@gmail.com>
2019-03-17 22:08:47 -0500

    ci test

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://dbu:dbpass1@ds155665.mlab.com:55665/capnode"
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-capnode';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://dbu:dbpass1@ds157097.mlab.com:57097/test-capnode'
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET || 'default'
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
